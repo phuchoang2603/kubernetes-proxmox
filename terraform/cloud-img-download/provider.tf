fa7a6a91f80ea82dddb19a62b32e44b29d4ee247
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-09-20 18:41:19 -0400

    configure terraform aws remote state

--- terraform/cloud-img-download/provider.tf
+++ terraform/cloud-img-download/provider.tf
@@ -1,10 +1,12 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
       version = "0.77.1"
     }
   }
+  backend "s3" {}
 }
 
 provider "proxmox" {
@@ -14,5 +16,3 @@ provider "proxmox" {
   username = var.proxmox_username
   password = var.proxmox_password
 }
-
-
