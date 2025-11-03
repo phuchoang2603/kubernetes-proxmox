2062fe1ddc1a3e07cc77afbcb045fd5cad29c58b
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-03 15:26:13 -0500

    simplify master script

--- scripts/master.sh
+++ scripts/master.sh
@@ -2,35 +2,6 @@
 
 set -e
 
-# Define supported optional features (case-sensitive)
-SUPPORTED_FEATURES=("longhorn" "ssl" "kube_vip" "argocd")
-
-# Initialize feature flags: default is enabled
-declare -A FEATURES
-for feature in "${SUPPORTED_FEATURES[@]}"; do
-  FEATURES["$feature"]=true
-done
-
-# Parse arguments
-for arg in "$@"; do
-  if [[ "$arg" == --skip-* ]]; then
-    feature="${arg/--skip-/}"
-
-    if [[ -v FEATURES["$feature"] ]]; then
-      FEATURES["$feature"]=false
-      echo "Skipping feature: $feature"
-    fi
-  else
-    echo "Unknown option: $arg"
-    exit 1
-  fi
-done
-
-# Export feature environment variables
-for key in "${!FEATURES[@]}"; do
-  export "$key"="${FEATURES[$key]}"
-done
-
 # Pre-setup, install kubectl, helm, and kubectx and load .env
 source ./pre-setup.sh
 source ../.env
@@ -50,24 +21,11 @@ cd ../ansible/
 source ../.venv/bin/activate
 source ../.env
 
-# Build skip-tags string based on disabled features
-TAGS_TO_SKIP=""
-for key in "${!FEATURES[@]}"; do
-  if [[ "${FEATURES[$key]}" != true ]]; then
-    TAGS_TO_SKIP="${TAGS_TO_SKIP}${key},"
-  fi
-done
-
-# Trim trailing comma
-TAGS_TO_SKIP="${TAGS_TO_SKIP%,}"
-
-# Run playbooks with skip-tags
-if [[ -n "$TAGS_TO_SKIP" ]]; then
-  ansible-playbook prepare-local.yaml --skip-tags "$TAGS_TO_SKIP"
-  ansible-playbook site.yaml --skip-tags "$TAGS_TO_SKIP"
-else
-  ansible-playbook prepare-local.yaml
-  ansible-playbook site.yaml
-fi
+# Run all playbooks without skipping any tags
+ansible-playbook prepare-local.yaml
+ansible-playbook site.yaml
 
 cd -
+
+# Rename kubectl environment
+kubectx $TF_VAR_env=default
