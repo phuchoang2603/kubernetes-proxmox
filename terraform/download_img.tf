7ddcceddc185dfa09416da3c4fa9ab71b4fc03e8
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-13 03:46:02 -0500

    overwrite false

--- terraform/download_img.tf
+++ terraform/download_img.tf
@@ -3,6 +3,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   datastore_id = var.vm_datastore_id
   node_name    = var.vm_node_name
   file_name    = "${var.env}-noble-server-cloudimg-amd64.img"
+  overwrite    = false
 
   url = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
 }
