964243f3ca1632b6c7cd1a746c5b4502f5a2e223
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-14 04:43:06 -0500

    explicitly define safe variables on the backend s3 provider

--- terraform-provision/provider.tf
+++ terraform-provision/provider.tf
@@ -6,7 +6,19 @@ terraform {
       version = "0.77.1"
     }
   }
-  backend "s3" {}
+  backend "s3" {
+    bucket = "terraform"
+    key    = "admin.tfstate"
+    region = "us-east-1"
+    endpoints = {
+      s3 = "http://10.69.1.102:9000"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
+  }
 }
 
 provider "proxmox" {
