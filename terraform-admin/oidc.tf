4ccb8c92588fbf37f360427e56b666df3b13149c
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-14 06:36:51 -0500

    change bound_sub to use bound_claim

--- terraform-admin/oidc.tf
+++ terraform-admin/oidc.tf
@@ -6,22 +6,27 @@ resource "vault_jwt_auth_backend" "github_oidc" {
 }
 
 resource "vault_jwt_auth_backend_role" "github_actions_push_role" {
-  backend         = vault_jwt_auth_backend.github_oidc.path
-  role_name       = "${var.env}-github-actions-push-role"
-  role_type       = "jwt"
-  token_policies  = [vault_policy.vault_env_policy.name, vault_policy.shared_policy.name]
-  bound_audiences = ["https://github.com/${var.github_organization}"]
-  user_claim      = "actor"
-
-  bound_subject = "repo:${var.github_organization}/${var.github_repository}:ref:refs/heads/${var.github_branch}"
+  backend           = vault_jwt_auth_backend.github_oidc.path
+  role_name         = "${var.env}-github-actions-push-role"
+  role_type         = "jwt"
+  token_policies    = [vault_policy.vault_env_policy.name, vault_policy.shared_policy.name]
+  bound_audiences   = ["https://github.com/${var.github_organization}"]
+  bound_claims_type = "glob"
+  bound_claims = {
+    "sub" = "repo:${var.github_organization}/${var.github_repository}:ref:refs/heads/${var.github_branch}"
+  }
+  user_claim = "actor"
 }
 
 resource "vault_jwt_auth_backend_role" "github_actions_pr_role" {
-  backend         = vault_jwt_auth_backend.github_oidc.path
-  role_name       = "${var.env}-github-actions-pr-role"
-  role_type       = "jwt"
-  token_policies  = [vault_policy.vault_env_policy.name, vault_policy.shared_policy.name]
-  bound_audiences = ["https://github.com/${var.github_organization}"]
-  user_claim      = "actor"
-  bound_subject   = "repo:${var.github_organization}/${var.github_repository}:pull_request"
+  backend           = vault_jwt_auth_backend.github_oidc.path
+  role_name         = "${var.env}-github-actions-pr-role"
+  role_type         = "jwt"
+  token_policies    = [vault_policy.vault_env_policy.name, vault_policy.shared_policy.name]
+  bound_audiences   = ["https://github.com/${var.github_organization}"]
+  bound_claims_type = "glob"
+  bound_claims = {
+    "sub" = "repo:${var.github_organization}/${var.github_repository}:pull_request"
+  }
+  user_claim = "actor"
 }
