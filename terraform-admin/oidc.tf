a409833b1af47d34be8ed8785056eb3059e93a4c
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-14 06:23:02 -0500

    change to bound_subject

--- terraform-admin/oidc.tf
+++ terraform-admin/oidc.tf
@@ -5,7 +5,6 @@ resource "vault_jwt_auth_backend" "github_oidc" {
   oidc_discovery_url = var.github_identity_provider
 }
 
-# ROLE 1: For PUSHES to 'master'
 resource "vault_jwt_auth_backend_role" "github_actions_push_role" {
   backend         = vault_jwt_auth_backend.github_oidc.path
   role_name       = "${var.env}-github-actions-push-role"
@@ -17,7 +16,6 @@ resource "vault_jwt_auth_backend_role" "github_actions_push_role" {
   bound_subject = "repo:${var.github_organization}/${var.github_repository}:ref:refs/heads/${var.github_branch}"
 }
 
-# ROLE 2: For PULL REQUESTS *targeting* 'master'
 resource "vault_jwt_auth_backend_role" "github_actions_pr_role" {
   backend         = vault_jwt_auth_backend.github_oidc.path
   role_name       = "${var.env}-github-actions-pr-role"
@@ -25,12 +23,5 @@ resource "vault_jwt_auth_backend_role" "github_actions_pr_role" {
   token_policies  = [vault_policy.vault_env_policy.name, vault_policy.shared_policy.name]
   bound_audiences = ["https://github.com/${var.github_organization}"]
   user_claim      = "actor"
-
-  bound_claims = {
-    "repository" = "${var.github_organization}/${var.github_repository}"
-    "event_name" = "pull_request"
-    "base_ref"   = var.github_branch
-  }
-
-  bound_claims_type = "glob"
+  bound_subject   = "repo:${var.github_organization}/${var.github_repository}:pull_request"
 }
