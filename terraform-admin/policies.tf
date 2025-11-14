a0aaf08b992fcad9e33c19c6de5ab5b0f32508bd
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-11-14 08:05:31 -0500

    test exporttoken

--- terraform-admin/policies.tf
+++ terraform-admin/policies.tf
@@ -4,6 +4,11 @@ resource "vault_policy" "vault_env_policy" {
     path "kv/${var.env}/data/*" {
       capabilities = ["read", "list"]
     }
+
+    # Grant permission to sign keys using a specific role
+    path "${vault_mount.ssh_client_signer.path}/sign/github-runner" {
+      capabilities = ["update"]
+    }
   EOT
 }
 
